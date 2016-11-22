856a328dc27e97c85274dc238a2bb7156b3751ed
burntoptics <burntoptiocs@yahoo.com>
2016-11-22 09:01:09 -1000

    adding all the phase 1 base models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :ratings
+  resources :criteria
+  resources :view_statuses
+  resources :items
+  resources :topics
+  resources :channels
+  resources :orgs
+  resources :users
   get 'welcome_page/Welcome'
   root 'welcome_page#welcome'
 end
